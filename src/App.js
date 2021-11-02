ec0bc6e92e3b63a214bbf537134755d0781ca05f
Max <epicycloidus@mail.com>
2021-11-02 22:31:56 +0300

    Легкая верстка

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Начнем</h1>
+      <div className="container">
+
+          <h1 className="h1">Список постов</h1>
+
+          <form className="form">
+              <input className="input" placeholder='Введите текст поста'/>
+              <button className="button">Отправить</button>
+          </form>
+
+          <div className="posts">
+            <div className="post">
+              <div className="post_text">
+                  <input type='checkbox'/>
+                  <div>Текст поста</div>
+              </div>
+              <button className="button">Удалить</button>
+            </div>
+          </div>
+          
+      </div>
     </div>
   );
 }
